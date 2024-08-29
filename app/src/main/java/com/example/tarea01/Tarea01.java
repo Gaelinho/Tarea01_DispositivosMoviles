e5dbccdabb67e0d4f04c09b09c10af4118683ca6
Gaelinho <gaelgaag10@ciencias.unam.mx>
2024-08-28 20:18:21 -0600

    Agregue dibujo

---
+++ app/src/main/java/com/example/tarea01/Tarea01.java
@@ -0,0 +1,13 @@
+package com.example.tarea01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tarea01 extends Activity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
